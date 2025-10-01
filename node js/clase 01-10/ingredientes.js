1ccd5d167243c7dfbade20ed38f379743f822260
Santiago Topham <stopham@practia.uy>
2025-10-01 09:34:05 +0200

    pizza excersise

---
+++ node js/clase 01-10/ingredientes.js
@@ -0,0 +1,61 @@
+module.exports = {
+	DoughTypes: [
+		{
+			Option: "A",
+			Name: "Masa normal",
+			Price: 3,
+		},
+		{
+			Option: "B",
+			Name: "Masa espelta",
+			Price: 3.5,
+		},
+		{
+			Option: "C",
+			Name: "Masa sin gluten",
+			Price: 3.5,
+		},
+	],
+	Toppings: [
+		{
+			Option: 1,
+			Name: "tomate",
+			Price: 1.5,
+		},
+		{
+			Option: 2,
+			Name: "mozzarella",
+			Price: 2.8,
+		},
+		{
+			Option: 3,
+			Name: "champiñones",
+			Price: 2.5,
+		},
+		{
+			Option: 4,
+			Name: "gorgonzola",
+			Price: 3.8,
+		},
+		{
+			Option: 5,
+			Name: "parmesano",
+			Price: 3.5,
+		},
+		{
+			Option: 6,
+			Name: "bacon",
+			Price: 3.4,
+		},
+		{
+			Option: 7,
+			Name: "atún",
+			Price: 4.5,
+		},
+		{
+			Option: 8,
+			Name: "duja",
+			Price: 3.8,
+		},
+	],
+};
---
+++ node js/clase 01-10/pizzeria.js
@@ -0,0 +1,83 @@
+// ENUNCIADO
+
+// 1 - Mostrar el menú con las opciones:
+
+// Menú de la pizzeria
+// ===================
+
+// A. Masa normal
+// B. Masa espelta
+// ....
+// 1. tomate
+// ...
+
+let title = "\n Menú de la pizzeria";
+const ingredientes = require("./ingredientes.js");
+
+console.log(title);
+console.log("=".repeat(title.length), "\n");
+
+console.log("Tipo de masa: \n");
+for (const currentDough of ingredientes.DoughTypes) {
+	console.log(`${currentDough.Option}${currentDough.Name}`);
+}
+
+console.log("\n Toppings: \n");
+ingredientes.Toppings.forEach(function (currentTop, i) {
+	console.log(`${i}. ${currentTop.Name}`);
+});
+
+// Al ejecutar el fichero el usuario hace la elección
+// de los ingredientes, pero hay que respetar estas
+// condiciones:
+
+// 1) Tiene que elegir un tipo de masa, pero solo uno
+// 2) Tiene que elegir como mínimo tres ingredientes y
+// como máximo cinco. Si elige menos, mostraremos
+// un mensaje de error, si eleige más de 5, sólo
+// utilizaremos los cinco primeros.
+
+function GetDoughByOption(option) {
+	return ingredientes.DoughTypes.find((x) => x.Option == option);
+}
+
+function GetToppingsByOption(option) {
+	return ingredientes.Toppings.find((x) => x.Option == option);
+}
+
+// Ejemplo:
+// node ./pizzeria.js C 1 3 5 6
+const baseProcessPosition = 2;
+const selectedDoughType = GetDoughByOption(process.argv[baseProcessPosition]);
+const selectedToppings = [];
+let toppingsMessage = "";
+const minToppings = 3;
+const maxToppings = 5;
+
+for (let i = 1; i <= maxToppings; i++) {
+	let currentTop = GetToppingsByOption(process.argv[baseProcessPosition + i]);
+
+	if (currentTop != undefined && currentTop != null) {
+		selectedToppings.push(currentTop);
+		toppingsMessage += `${currentTop.Name}, `;
+	}
+}
+
+toppingsMessage = toppingsMessage.slice(0, toppingsMessage.length - 2);
+
+// Con esto, indicaremos al usuario que ha elegido y cuanto cuesta.
+// "Preparamos tu pizza con masa sin gluten, tomate, champiñones, parmesano, bacon."
+// "Precio: 14.40€"
+
+if (
+	selectedToppings.length < minToppings ||
+	selectedToppings.length > maxToppings
+) {
+	console.log(
+		`\n Debe seleccionar minimo ${minToppings} toppings y maximo ${maxToppings}`
+	);
+} else {
+	console.log(
+		`\n Preparamos tu pizza con ${selectedDoughType.Name}, ${toppingsMessage}.`
+	);
+}
